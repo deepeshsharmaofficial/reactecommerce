65f55d9facc649beaf2265ac319a4cd8e618039b
Deepesh Sharma <deep92112018@gmail.com>
2023-09-17 00:35:32 +0530

    Complete Footer Component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {GlobalStyle} from "./GlobalStyle";
 
 import { ThemeProvider } from "styled-components";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const App = () => {
 
@@ -56,6 +57,7 @@ const App = () => {
             <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<ErrorPage />} />
           </Routes>
+          <Footer />
       </BrowserRouter>
     </ThemeProvider>
   )
