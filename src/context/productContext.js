d824da6d02a9264d50fd85afdffbac2fc7642ffc
Deepesh Sharma <deep92112018@gmail.com>
2023-09-17 14:02:52 +0530

    Fetch API data using Axios

--- src/context/productContext.js
+++ src/context/productContext.js
@@ -4,13 +4,31 @@
  * Consumer (Consumer is lengthy so resolve this resolve we will use "useContext Hook")
  */
 
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
 
 // 1st Step -> Create a Context
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products"
+
 // 2nd Step -> Create Provider
 const AppProvider = ({ children }) => {
+    
+    const getProducts = async (url) => {
+        try {
+            const response = await axios.get(url);
+            const products = await response.data;
+            console.log("API Products ---> ", products);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getProducts(API);
+    }, [])
+
     return (
         <AppContext.Provider value="Deepesh">
             {children}
