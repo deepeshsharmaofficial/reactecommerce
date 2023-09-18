f4ee95587a5d9a354bd7e2212e89effaf65df761
Deepesh Sharma <deep92112018@gmail.com>
2023-09-18 16:08:01 +0530

    Get API Data with ID and Destructure in Single Product Page

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -1,7 +1,40 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { useProductContext } from "./context/productContext";
+
+const API = "https://api.pujakaitem.com/api/products";
 
 const SingleProduct = () => {
-  return <Wrapper></Wrapper>;
+
+  const { getSingleProduct, isSingleProductLoading, singleProduct } = useProductContext();
+
+  const { id } = useParams();
+  console.log("Single Product id ---> ", id);
+
+  // destructuring
+  const {
+    id: alias,
+    name,
+    company,
+    price,
+    description,
+    category,
+    stock,
+    reviews,
+    stars,
+    image,
+  } = singleProduct;
+
+  useEffect(() => {
+    getSingleProduct(`${API}?id=${id}`);
+  }, []);
+
+  return (
+    <Wrapper>
+      <h1>{name}</h1>
+    </Wrapper>
+  )
 };
 
 const Wrapper = styled.section`
